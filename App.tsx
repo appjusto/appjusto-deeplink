d21ddf5a2f9c483fdc973407d6d8778dfba14a03
Pedro Andrade <pdandradeb@gmail.com>
2022-01-28 10:58:36 -0300

    configuring deeplinks

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Linking from "expo-linking";
 
 export default function App() {
+  const deeplink = Linking.useURL();
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Deeplink: {deeplink} </Text>
       <StatusBar style="auto" />
     </View>
   );
